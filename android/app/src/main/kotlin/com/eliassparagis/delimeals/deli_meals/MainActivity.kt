020d1c58130fdfd5fefc36af1379444f1e97c77f
Elias Sparagis <esparagis@gmail.com>
2022-05-03 14:41:44 +0300

    initial commit

---
+++ android/app/src/main/kotlin/com/eliassparagis/delimeals/deli_meals/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.eliassparagis.delimeals.deli_meals
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/categories_screen.dart
@@ -0,0 +1,29 @@
+import 'package:deli_meals/category_item.dart';
+
+import './dummy_data.dart';
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeals'),
+      ),
+      body: GridView(
+        children: DUMMY_CATEGORIES.map((catData) {
+          return CategoryItem(
+            title: catData.title,
+            color: catData.color,
+          );
+        }).toList(),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+      ),
+    );
+  }
+}
